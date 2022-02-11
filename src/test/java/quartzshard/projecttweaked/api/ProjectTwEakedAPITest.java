65ebd33b5a1feec4938bfdbfc5b0a9c99bde3f9e
quartzshard <quartzshard@quartzshard.com>
2022-02-11 22:35:47 +0000

    Test example

--- src/test/java/quartzshard/projecttweaked/api/ProjectTwEakedAPITest.java
+++ src/test/java/quartzshard/projecttweaked/api/ProjectTwEakedAPITest.java
@@ -6,6 +6,14 @@ import org.junit.Test;
 
 public class ProjectTwEakedAPITest
 {
+	
+	//For Sol: Uncomment this and ./gradlew test to see why these are useful
+	// https://www.javatpoint.com/assertion-in-java
+	//@Test
+	//public void testTest() throws Exception
+	//{
+	//	assertNotNull(null); //This can be anything that asserts something, to check something was returning the correct value of 1024, you could `assert return1024()=1024`
+	//}
 
 	@Test
 	public void testGetEMCProxy() throws Exception
